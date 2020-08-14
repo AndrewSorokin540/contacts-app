23392f562512e87df05693725c40cac3ea5166bd
Andrey Sorokin <andrewsorokin540@gmail.com>
2020-08-14 20:30:24 +0700

    logout

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,17 +2,30 @@ import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { Layout, Menu } from 'antd';
 import { CurrentUserContext } from 'contexts';
+import { useLocalStorage } from 'hooks';
 const { Header: AntHeader } = Layout;
 
 const Header = () => {
-    const [currentUserState] = useContext(CurrentUserContext);
+    const [, setToken] = useLocalStorage('token');
+    const [currentUserState, setCurrentUserState] = useContext(CurrentUserContext);
+    const { isLoggenIn } = currentUserState;
+
+    const onExit = () => {
+        setCurrentUserState({
+            currentUser: null,
+            isLoggenIn: false,
+            loading: false
+        });
+        setToken('');
+    }
     return (
         <AntHeader>
-            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-                <Menu.Item key="1"><Link to='/'>Контакты</Link></Menu.Item>
-                <Menu.Item key="2"><Link to='/login'>Вход</Link></Menu.Item>
-                <Menu.Item key="3"><Link to='/register'>Регистрация</Link></Menu.Item>
-                {currentUserState.currentUser && <Menu.Item key="4">{currentUserState.currentUser.name}</Menu.Item>}
+            <Menu theme="dark" mode="horizontal">
+                <Menu.Item key="1">{isLoggenIn ? 'Залогинен' : 'Не залогинен'}</Menu.Item>
+                <Menu.Item key="2"><Link to='/'>Контакты</Link></Menu.Item>
+                <Menu.Item key="3" onClick={() => onExit()}>Выйти</Menu.Item>
+                <Menu.Item key="4"><Link to='/login'>Вход</Link></Menu.Item>
+                <Menu.Item key="5"><Link to='/register'>Зарегистрироваться</Link></Menu.Item>
             </Menu>
         </AntHeader>
     )
